466483a4d78f2322e0e6127cb035a8980002a27c
kevynkrancenblumsce <75220592+kevynkrancenblumsce@users.noreply.github.com>
2020-12-31 12:19:45 +0200

    31.12

--- Staticov/views.py
+++ Staticov/views.py
@@ -111,18 +111,22 @@ def get_data_test(request):
 def CHANGE_PASSWORD_TEST(request):
     result = []
     cursor.execute("SELECT * FROM `registerform`")
-    data = cursor.fetchall()
+    data = cursor.fetchall()    
     if request.method=='POST':
-         useridtest=request.POST.get('userid')
-         passwordtest=request.POST.get('password')    
+         useridtest=request.POST.get('taz')
+         passwordtest=request.POST.get('password') 
     for item in data:
-        ID,name,taz,password,Type = item
+        ID,name,taz,password,type = item
         if useridtest==taz:
-           IDtest=ID
-           cursor.execute(" UPDATE `registerform` SET `password` = '12323312' WHERE `registerform`.`ID` = 'ID';")
-        return index(request)
-    messages.error(request,'הפרטים שהוזנו לא נמצאים במערכת')   
-    return registration(request) 
+            cursor.execute("UPDATE `registerform` SET password = '%s' WHERE registerform.ID = '%s'"%(passwordtest,taz))
+            db_connection.commit()
+            return index(request)
+    else :
+        messages.error(request,'הפרטים שהוזנו לא נמצאים במערכת')   
+        return registration(request)
+    
+        
+       
 
 
 def datapatient(request):
